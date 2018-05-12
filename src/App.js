a1dc2225e4485a1989b75f9016b6afdc33467b60
Richa Verma <richaverma@Richas-MacBook-Air.local>
2018-05-12 16:34:25 +0530

    landing page - fixed navbar and all css issues

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Navbar, NavItem, Nav, Image, Button, Grid, Row, Col, Collapse, Brand, Toggle} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {Grid, Row, Col, Image} from 'react-bootstrap';
+import { Link } from 'react-router';
 
 class App extends Component {
   render() {
@@ -18,26 +18,16 @@ class App extends Component {
 class Header extends Component {
    render() {
       return (
-        <Navbar fluid collapseOnSelect>
-            <Navbar.Header className="landing-navbar-header">
-              <Image className="img-responsive" src="/HODLTAB.png" />
-            </Navbar.Header>
-
-            <Navbar.Toggle />
-          <Navbar.Collapse>
-            <Nav pullRight className="landing-navbar-links">
-              <NavItem eventKey={1} href="#">
-                CONTACT
-              </NavItem>
-              <NavItem eventKey={2} href="#">
-                LOGIN
-              </NavItem>
-              <NavItem eventKey={3} href="#">
-                SIGNUP
-              </NavItem>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+        <nav className="navbar navbar-default landing-navbar">
+                <div className="navbar-header landing-navbar-brand">
+                <img className="img-responsive" src="/HODLTAB.png" />
+              </div>
+              <ul className="nav navbar-right landing-navbar-links">
+                <li><a className="landing-navbar-links">CONTACT</a></li>
+                <li><a className="landing-navbar-links">LOG IN</a></li>
+                <li><button className="sign-up-button">SIGN UP </button></li>
+              </ul>
+            </nav>
       );
    }
 }
@@ -46,15 +36,15 @@ class Content extends React.Component {
       return (
         <Grid className="container-custom">
           <Row>
-            <Col sm={5}>
+            <Col sm={6}>
               <code><p className="landing-content-header">The Crypto Traders Best Kept Secret</p></code>
               <code><p className="landing-content">HODLTab is a new-tab Chrome extension that aggregates best content from 250+ sources for coins you want to track</p></code>
                 <Row>
-                <Col sm={8}><button className="landing-download-button" href="#"><Image src="/chrome.png" /><p>     Download now</p></button></Col>
-                <Col sm={4}><a className="landing-preview" href="#">See preview</a></Col>
+                <Col sm={6}><button className="landing-download-button" href="#"><img src="/chrome.png" /><p className="download-button-content">Download now</p></button></Col>
+                <Col sm={6} style={{paddingTop:'15px'}}><a href="#" className="landing-preview">See preview</a></Col>
                 </Row>
             </Col>
-            <Col sm={7}>
+            <Col sm={6}>
               <Image className="landing-image img-responsive" src="/Rectangle 2.png" />
             </Col>
           </Row>
