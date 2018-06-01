75a80091db7b98f591ab42fae2a4617a22294d19
Richa Verma <richaverma@Richas-MacBook-Air.local>
2018-06-01 21:22:27 +0530

    prod callback url

--- src/Auth/auth0-config.js
+++ src/Auth/auth0-config.js
@@ -1,7 +1,7 @@
 export const AUTH_CONFIG = {
   domain: 'hodltab.auth0.com',
   clientId: 'bcvfRNa8uDnGA2IiXXX2y0fG7z69bZTq',
-  callbackUrl: 'http://localhost:3000/callback',
+  callbackUrl: 'http://139.59.76.115/callback',
   dbConnectionName: 'Username-Password-Authentication',
   container: 'auth0-login-container'
 }
