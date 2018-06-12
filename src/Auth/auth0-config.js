8bf85602fd5ab71bd6e294f07efb2157d748e15b
Richa Verma <richaverma@Richas-MacBook-Air.local>
2018-06-12 21:10:48 +0530

    adding domain for prod

--- src/Auth/auth0-config.js
+++ src/Auth/auth0-config.js
@@ -7,7 +7,7 @@ export const AUTH_CONFIG = (process.env.NODE_ENV === 'development') ? {
 } : {
   domain: 'hodltab.auth0.com',
   clientId: 'bcvfRNa8uDnGA2IiXXX2y0fG7z69bZTq',
-  callbackUrl: 'http://139.59.76.115/callback',
+  callbackUrl: 'https://hodltab.com/callback',
   dbConnectionName: 'Username-Password-Authentication',
   container: 'auth0-login-container'
 }
