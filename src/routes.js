94020486cc2b1d0834ba89123bef9b40f658228b
Richa Verma <richaverma@Richas-MacBook-Air.local>
2018-06-18 15:23:26 +0530

    fixing routes

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,6 @@ import App from './App';
 import Onboarding from './components/onboarding.js';
 import history from './history';
 import Private from './components/Home';
-import Login from './Auth/Login';
-import Logout from './Auth/Logout';
 import SelectCrypto from './components/SelectCrypto';
 import CustomizeFeed from './components/CustomizeFeed';
 import Home from './components/Home';
@@ -25,9 +23,7 @@ export const makeMainRoutes = () => {
       <Router history={history} component={App}>
             <Switch>
               <Route exact path="/" component={App} />
-              <Route path="/login" component={Login} />
               <Route path="/select_crypto" component={SelectCrypto} onEnter={requireAuth}/>
-              <Route path="/logout" component={Logout} />
               <Route path="/customize_feed" component={CustomizeFeed} />
               <Route path="/home" component={Home} onEnter={requireAuth}/>
               <Route path="/callback" component={Callback} />
