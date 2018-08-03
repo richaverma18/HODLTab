d1c4ddf2782bd853a74850a1edb326e5d8ec2e11
Richa Verma <richaverma@Richas-MacBook-Air.local>
2018-08-03 16:02:45 +0530

    minor fic

--- src/utils/Formatter.js
+++ src/utils/Formatter.js
@@ -1,4 +1,7 @@
 export function formatToUnits(number) {
+  if(number === '' || number === null){
+    return '';
+  }
   const abbrev = ['', 'K', 'M', 'B', 'T'];
   const order = Math.min(Math.floor(Math.log10(Math.abs(number)) / 3), abbrev.length - 1);
   const suffix = abbrev[order];
